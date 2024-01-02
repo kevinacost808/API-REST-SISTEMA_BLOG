fb2b2595faf72989a1694fcdb08335b31083da65
kevin acosta <kevinacost808@gmail.com>
2024-01-01 23:55:13 -0500

    creacion de controlador y prueba del metodo post en postman

--- src/main/java/com/sistema/blog/demo/controlador/PublicacionControlador.java
+++ src/main/java/com/sistema/blog/demo/controlador/PublicacionControlador.java
@@ -1,7 +1,13 @@
 package com.sistema.blog.demo.controlador;
 
+import com.sistema.blog.demo.dto.PublicacionDto;
+import com.sistema.blog.demo.entidades.Publicacion;
 import com.sistema.blog.demo.servicio.PublicacionServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicacionControlador {
     @Autowired
     private PublicacionServicio publicacionServicio;
+
+    @PostMapping
+    public ResponseEntity<PublicacionDto> guardarPublicacion(@RequestBody PublicacionDto publicacionDto){
+        return new ResponseEntity<>(publicacionServicio.crearPublicacion(publicacionDto), HttpStatus.CREATED);
+    }
 }
